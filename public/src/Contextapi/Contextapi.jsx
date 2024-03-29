9916d5991bb9b87f94446d22df893461a6618f99
avinashraj-151 <avinashrajraj628@gmail.com>
2024-03-29 15:09:13 +0530

    createContextapi

---
+++ public/src/Contextapi/Contextapi.jsx
@@ -0,0 +1,8 @@
+import { createContext } from "react";
+
+export const Context = createContext();
+const ContextProvider = (props) => {
+  return <Context.Provider>{props.children}</Context.Provider>;
+};
+
+export default ContextProvider;
--- public/src/index.js
+++ public/src/index.js
@@ -3,5 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+import ContextProvider from "./Contextapi/Contextapi";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>
+);
